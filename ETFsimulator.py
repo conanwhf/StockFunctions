8309700b47918510e4d430ede39781f0871edefd
Conan <conan.whf@gmail.com>
2018-02-08 19:23:28 +0800

    update ETF simulator

--- ETFsimulator.py
+++ ETFsimulator.py
@@ -4,27 +4,39 @@ import tushare as ts
 import requests
 from StockAPIs import *
 import itertools
+import json
+
+HistoryDataFile = "history.json"
 
 max_decrease = 0
 min_increase = 0
 
-def get_history_data(allcode, years):
-	res = {}
-	for i in allcode:
-		res[i] = []
-		for year in years:
-			start = datetime.date(year, 1, 1)
-			end = datetime.date(year, 12, 31)
-			data = ts.get_k_data(allcode[i], start=start.strftime('%Y-%m-%d'), end=end.strftime('%Y-%m-%d'))
-			try:
-				t1 = data.open.iloc[0]
-				t2 = data.close.tail(1).iloc[0]
-			except:
-				print("%d年%s数据不足或有误，按无收益计算"%(year, i))
-				t1=t2=1
-			#保存当年收益率
-			res[i] += [(t2-t1)/t1]
-		#print("%s年 %s 收益率为： %s\n" %(years, i, res[i]))
+def get_history_data(allcode, years, byFile=False):	
+	if byFile==True:
+		f = open(HistoryDataFile, 'r' ,encoding="utf8")
+		res = json.loads(f.read())
+		f.close()
+		return res
+	else:
+		res = {}
+		for i in allcode:
+			res[i] = []
+			for year in years:
+				start = datetime.date(year, 1, 1)
+				end = datetime.date(year, 12, 31)
+				data = ts.get_k_data(allcode[i], start=start.strftime('%Y-%m-%d'), end=end.strftime('%Y-%m-%d'))
+				try:
+					t1 = data.open.iloc[0]
+					t2 = data.close.tail(1).iloc[0]
+				except:
+					print("%d年%s数据不足或有误，按无收益计算"%(year, i))
+					t1=t2=1
+				#保存当年收益率
+				res[i] += [(t2-t1)/t1]
+		#将获取的数据保存成文件
+		f = open(HistoryDataFile, 'w', encoding="utf8")
+		f.write(json.dumps(res, ensure_ascii=False))
+		f.close()
 	return res	
 	
 	
@@ -56,15 +68,25 @@ def cal_case_increase(persent, increase, count):
 
 def ETFsimulator(allcode, years):
 	#计算历年不同指数的收益率
-	increase = get_history_data(allcode, years)
+	increase = get_history_data(allcode, years, byFile=True)
 	print(increase)
-	plot_data(years, data=list(increase.values()),labels=list(increase.keys()), title=u'历年不同指数的收益率', show=False)
-
+	plot_data(years, data=list(increase.values()),labels=list(increase.keys()), title=u'历年不同投资方向的收益率', show=False)
+	#计算历年不同指数的累计现值
+	value ={}
+	for i in increase:
+		temp = 1.0
+		value[i] =[]
+		for j in increase[i]:
+			temp += temp*j
+			value[i] += [temp]
+	print(value)
+	plot_data(years, data=list(value.values()),labels=list(value.keys()), title=u'历年投资指数的累计现值（基准为1.0）', show=False)
+	
 	#生成所有case，将100%分配给不同的指数
 	x = itertools.combinations_with_replacement(allcode.keys(), 10)
 	res = []
+	solution = []
 	for case in list(x):
-		#persent = dict(zip(allcode.keys(), [0]*len(allcode)))
 		persent = dict.fromkeys(allcode.keys(), 0.0)
 		for i in case:
 			persent[i] += 10
@@ -74,18 +96,44 @@ def ETFsimulator(allcode, years):
 			print(persent)
 			print(temp)
 			res += [temp]
+			solution += [persent]
 	print(len(res))
-	#plot_data(years, data=res, labels=['???']*len(res),title=u'符合条件的case', show=True)
+	if res==[]:
+		print("没有找到符合条件的选择，退出")
+		return []
+	#将配置内容作为每个case的标签
+	labels = []
+	for temp in solution:
+		st = ''
+		for j in temp:
+			if temp[j]>0:
+				st += "%s:%d "%(j,int(temp[j]))
+		labels += [st]
+	plot_data(years, data=res, labels=labels, title=u'符合条件的case', show=True)
+	return solution
 
 
 if __name__ == "__main__":
-	allcode ={'黄金': '518800',	'沪深300': '510300',	
-				'创业板': '159915', '央企': '510060', '医药': '512010', 
-				'上证50': '000016',	'纳指': '513100',	'标普': '513500', 
-				'恒指': '513600'}
-				#'中证500': '510500',	
-	year_range=range(2013,2018)
-	max_decrease = -5 #挑选时允许的最大亏损率（5%）
-	min_increase = 18 #挑选时允许的最小平均收益率（18%）
+	allcode ={	'上证':'000001', 	'深成指':'399001', 	'创业板': '159915', 
+				'沪深300': '399300', '中证500': '000905', '上证50': '000016',
+				'上证中小':'000046', 	'上证央企': '000042', '国企':'000056', 
+				'TMT50':'399610', 	'白酒': '399997', 	'医药': '000037',
+				'能源':'000032', 	'地产':'000006', 	'工业':'000034',
+				'金融':'000038', 
+				'纳指': '513100', 	'标普': '513500', 	'恒指': '513600',
+				'国债':'000012', 	'黄金':'518800'
+				}
+			#'上证':'000001', '深成指':'399001', '创业板': '159915', 
+			#'沪深300': '399300', '中证500': '000905', '上证50': '000016',
+			#'上证中小':'000046', '上证央企':'000042', '国企':'000056', 
+			#'TMT50':'399610', '白酒': '399997', '医药': '000037',
+			#'能源':'000032', '地产':'000006', '工业':'000034',
+			#'金融':'000038', 
+			#'纳指': '513100', '标普': '513500', '恒指': '513600',
+			#'国债':'000012', '黄金':'518800'	
+		
+	year_range=range(1998,2019)
+	max_decrease = -5 #挑选时允许的最大亏损率（3%）
+	min_increase = 10 #挑选时允许的最小平均收益率（18%）
 	ETFsimulator(allcode, year_range)
 	
--- StockAPIs.py
+++ StockAPIs.py
@@ -5,6 +5,7 @@ import requests
 import tushare as ts
 import pandas as pd
 import matplotlib.pyplot as plt
+import random
 
 ''' 
 从tushare API 获取所有股票列表信息
@@ -57,13 +58,15 @@ def revise_date(start, end):
 对几个数组绘制折线图
 '''
 def plot_data(x=[], data=[[]], labels=[], title = "T=???", show=False):
-	colors="bgryckm"
-	plt.rcParams['font.sans-serif'] = ['SimHei']  # for Chinese characters
-	fig,ax = plt.subplots()
-	plt.title(title)
-	plt.grid(True)
 	#计算需要的中间变量
 	count = len(data)
+	if count == 0:
+		print("待绘制数据为空！")
+		return
+	if labels==[]:
+		print("自动生成数据的labels")
+		for i in range(count):
+			labels += ["%04d"%i]
 	if count!=len(labels):
 		print("数据名称和数据数组个数不符，无法绘制")
 		return
@@ -75,15 +78,45 @@ def plot_data(x=[], data=[[]], labels=[], title = "T=???", show=False):
 	dataMin = min(alldata)
 	dataMax = max(alldata)	
 	lenMax = max(length)
+	#生成颜色数据
+	if count<=7:
+		colors="bgryckm"
+	else:
+		colors = []
+		for i in range(count):
+			random.seed()
+			(r,g,b)=random.randint(0,0x7f), random.randint(0x7f,0xff), random.randint(0,0xff)
+			if i%3==0:
+				colors += [ '#%02x%02x%02x'%(r,g,b)]
+			if i%3==1:
+				colors += [ '#%02x%02x%02x'%(b,r,g)]
+			if i%3==2:
+				colors += [ '#%02x%02x%02x'%(g,b,r)]
+			#colors += [ '#%06x' %random.randint(0,0xf0f0f0)]
+	#折线图初始化配置	
+	#colors="bgryckm"
+	plt.rcParams['font.sans-serif'] = ['SimHei']  # for Chinese characters
+	plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+	fig,ax = plt.subplots()
+	plt.title(title)
+	plt.grid(True)
 	#定义x, y的坐标轴
 	ax.set_ylim([dataMin-(dataMax-dataMin)*0.1,dataMax+(dataMax-dataMin)*0.1])    
 	if x==[] or len(x)!=lenMax:
 		x= range(lenMax)
 	#绘制
 	for i in range(count):
-		plt.plot(x[0:length[i]], data[i], "o-",label=labels[i], color=colors[i%7])
+		plt.plot(x[0:length[i]], data[i], "o-",label=labels[i], color=colors[i])
 	plt.legend(loc='best')
 	plt.close(0)
 	if show==True:
 		plt.show()
 	return
+
+'''
+A=[1,2,3,4,5]
+B=[5,6,3,4,8,1,-2]
+C=[0.1, 0.4, 10]
+x=[4,5,6,7,8,9,10]
+plot_data(x, data=[B,C,A],labels=["B", "C","A"], title=u'测试', show=True)
+'''
